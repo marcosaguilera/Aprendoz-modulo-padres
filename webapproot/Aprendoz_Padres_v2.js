a13603dc55259eb3d7bf529b4a4996568e726c51
Marcos Aguilera Ely <maguilera@rochester.edu.co>
2014-11-26 08:27:20 -0500

    Export Project Work 25 Nov 2014

--- webapproot/Aprendoz_Padres_v2.js
+++ webapproot/Aprendoz_Padres_v2.js
@@ -11,7 +11,7 @@ dojo.declare("Aprendoz_Padres_v2", wm.Application, {
 	"name": "", 
 	"phoneGapLoginPage": "Login", 
 	"phoneMain": "", 
-	"projectSubVersion": "Alpha67", 
+	"projectSubVersion": "Alpha68", 
 	"projectVersion": 1, 
 	"sessionExpirationHandler": "nothing", 
 	"studioVersion": "6.6.0.RELEASE", 
